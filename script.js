b7ef044dbdd18a6ddf342832ffdde89e60e0fbdd
marcosvitordev <marcosvitorlima531@gmail.com>
2024-06-18 17:52:28 -0300

    Curriculo

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function sendEmail() {
 
 // Adiciona um evento de clique ao botão para chamar a função sendEmail
 document.getElementById("emailButton").addEventListener("click", sendEmail);
+
+
+document.getElementById('curriculoDL').addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = 'assets/Curriculo_Marcos-Vitor.pdf';  // Substitua pelo caminho do seu arquivo PDF
+    link.download = 'Curriculo_Marcos-Vitor.pdf';  // Nome do arquivo que será baixado
+    link.click();
+});
+
+function downloadPDF() {
+    console.log("certo")
+    const link = document.createElement('a');
+    link.href = 'assets/Curriculo_Marcos-Vitor.pdf';  // Substitua pelo caminho do seu arquivo PDF
+    link.download = 'Curriculo_Marcos-Vitor.pdf';  // Nome do arquivo que será baixado
+    link.click();
+}
